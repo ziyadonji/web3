04dfca336912e122ffcbadb47a8686a6aee1c1de
ziyad c <ziyad.c@ibm.com>
2022-05-31 02:05:18 +0530

    complete

---
+++ Bank.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.4.25;
+
+contract Bank {
+    int bal;
+    constructor() public{
+        bal =1;
+    }
+
+    function getBalance() view public returns(int){
+        return bal;
+    }
+
+    function deposit(int amount) public{
+        bal = bal + amount;
+    }
+
+    function withdraw (int amount) public {
+        bal = bal - amount;
+    }
+}
